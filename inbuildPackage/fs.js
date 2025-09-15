688762fc40bb2df3ee59191da153405cb5546983
benosolomon <45790644+benosolomon@users.noreply.github.com>
2025-09-15 16:34:53 +0530

    rename_file_with_fs

--- inbuildPackage/fs.js
+++ inbuildPackage/fs.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 
-fs.writeFile("sample.txt","Node is awesome.but sync function block in asynchronous",handleFileCreation);
+// fs.writeFile("sample.txt","Node is awesome.but sync function block in asynchronous",(err)=> {
+//  if(err)  throw err;
+// console.log("Created a file");
+// });
+// console.log("END");
+
+fs.rename("sample.txt","demo.txt",(err)=> {
+ if(err)  throw err;
+console.log("Append a file");
+});
 console.log("END");
 
-
-function handleFileCreation(err) {
-  if(err)  throw err;
-console.log("Created a file");
-console.log("END");
-}
